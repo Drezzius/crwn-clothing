0336e9e44446bce4e0dfc76b9de4671aed1ab336
Peter Ruzsom <Peter.Ruzsom@diconium.com>
2019-12-10 13:47:03 +0100

    added userTypes

--- src/redux/user/userActions.js
+++ src/redux/user/userActions.js
@@ -1,5 +1,9 @@
+import { UserActionTypes } from './userTypes';
+
+const { SET_CURRENT_USER } = UserActionTypes;
+
 const setCurrentUser = (user) => ({
-	type: 'SET_CURRENT_USER',
+	type: SET_CURRENT_USER,
 	payload: user
 });
 
